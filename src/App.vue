d0b37efa45af8284f561f7317acd576450eb4331
Aaron Maywood <am7274183@gmail.com>
2023-07-18 09:52:10 +0900

    カードの絵柄を担当するカードコンポーネントの作成

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <script setup>
+import Card from './components/Card.vue'
 </script>
 
 <template>
+	<div class="dealer">
+		<Card suit="♠" rank="1" isOpen="true"></Card>
+		<Card suit="♣" rank="10" isOpen="false"></Card>
+		<Card suit="◆" rank="11" isOpen="true"></Card>
+		<Card suit="♥" rank="13" isOpen="true"></Card>
+	</div>
 </template>
+
+<style scoped>
+.dealer {
+	display: flex;
+	flex-wrap: wrap;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  suit: String,	// ♠♥♣◆
+  rank: Number,	// 1〜13
+  isOpen: Boolean,
+})
+</script>
+
+<template>
+	<div class="card">
+		<p v-if="isOpen === 'true'">
+			<span v-if="rank === '1'">A</span>
+			<span v-else-if="rank === '11'">J</span>
+			<span v-else-if="rank === '12'">Q</span>
+			<span v-else-if="rank === '13'">K</span>
+			<span v-else>{{rank}}</span>
+			{{suit}}
+		</p>
+		<p v-else>？？？</p>
+	</div>
+</template>
+
+<style scoped>
+.card {
+	border: 1px solid;
+	border-radius: 10px;
+	width: 120px;
+	height: 180px;
+	background: lightyellow;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
